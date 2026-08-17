922fff9f9fafdb3e0ca32827fde8503dca6133b9
dyyoungg <yangdeyu@sensetime.com>
2026-08-17 16:45:00 +0800

    update sh

--- train_multi_node.sh
+++ train_multi_node.sh
@@ -1,8 +1,9 @@
 
 
-export WANDB_API_KEY="" # replace with your API
+export WANDB_API_KEY="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" # replace with your API
 export PYTHONPATH="/mnt/afs/yangdeyu/GameMLLM/VeOmni-Dev:$PYTHONPATH"
 export VEOMNI_USE_LIGER_KERNEL="1"
+export VIT_PROFILE="0"
 
 cd /mnt/afs/yangdeyu/GameMLLM/VeOmni-Dev/
 echo "=== Distributed Training Environment Variables ==="
@@ -20,5 +21,4 @@ torchrun \
     --master_port=$MASTER_PORT \
     --nproc_per_node $nproc_per_node \
     /mnt/afs/yangdeyu/GameMLLM/VeOmni-Dev/tasks/train_llavaomni.py \
-    /mnt/afs/yangdeyu/GameMLLM/VeOmni-Dev/exp_data/mmdata_1024_rank96/14B_qwen25encoder_fsdp2.yaml
-    # 2>&1 | tee /mnt/afs/yangdeyu/GameMLLM/VeOmni-Dev/train.log
+    /mnt/afs/yangdeyu/GameMLLM/VeOmni-Dev/exp_data/0808_audio_zh_all/14B_qwen25encoder_fsdp2_audiodown8.yaml
